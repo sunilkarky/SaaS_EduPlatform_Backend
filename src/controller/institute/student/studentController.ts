57740ba89d33d4ec729dd03d7e0c72d30530a095
sunilkarky <sunilkarki670@gmail.com>
2025-06-18 15:32:32 +0545

    student controller setup

---
+++ src/controller/institute/student/studentController.ts
@@ -0,0 +1,14 @@
+import { Response } from "express";
+import { IExtendedRequest } from "../../../middleware/type";
+
+class studentController{
+    static async getStudent(req:IExtendedRequest,res:Response){
+
+    }
+    static async getSingleStudent(req:IExtendedRequest,res:Response){
+
+    }
+    static async deleteStudent(req:IExtendedRequest,res:Response){
+
+    }
+}
